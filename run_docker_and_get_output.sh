13156d7261fc5d2007b9075e437a584d8fd09fe4
actes2 <101230792+actes2@users.noreply.github.com>
2024-06-24 08:40:16 -0500

    Update run_docker_and_get_output.sh

--- run_docker_and_get_output.sh
+++ run_docker_and_get_output.sh
@@ -1,3 +1,2 @@
-echo "Anything sent to actesco.org on 4469 will route to here"
-#docker run -p 4469:6614/udp -it --rm -w /app/ -v /copy_and_paste/sailboat/:/app temp_cont
+echo "Anything sent to our host on port 4469 will route to here"
 docker run -p 4469:6614/udp -it --rm -w /app/ -v ./:/app temp_cont
